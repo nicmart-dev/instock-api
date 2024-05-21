a5eee3bf90baf6f272199000399c13090ed5c3c6
Nicolas Martinez <nicmart@gmail.com>
2024-05-21 14:32:45 -0700

    add initial folder structure #RR-4

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const inventoryRoute = require("./routes/inventory");
+const warehousesRoute = require("./routes/warehouses");
+require("dotenv").config(); // load environment variables from a .env file into process.env
+
+const { PORT } = process.env; // destructuring assignment of PORT from process.env
+
+// middleware
+app.use(express.json()); // parses incoming requests specifically req.body
+app.use(cors()); // allow * / all to access our api. All domains, ips, ports
+
+app.use("/inventory", inventoryRoute);
+app.use("/warehouses", warehousesRoute);
+
+app.get("/", (_req, res) => {
+    res.send("<h1>Welcome to the InStock API server!</h1>");
+});
+
+// boots up the server and listens on a specified port number
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
