5629f75cd20cf62a98a54cd646b30a401b75e98c
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-05-22 13:29:16 -0700

    minor changes

--- controllers/warehouse-controllers.js
+++ controllers/warehouse-controllers.js
@@ -4,18 +4,18 @@ const knex = require('knex')(require('../knexfile'));
 
 const remove = async (req, res) => {
     try {
-        console.log('deleting id: ', req.params.id);
-        const rowsDeleted = await knex('warehouses')
+        console.log('Finding id: ', req.params.id);
+        const warehouseDeleted = await knex('warehouses')
             .where({ id: req.params.id })
             .delete();
 
-        if (rowsDeleted === 0) {
+        if (warehouseDeleted === 0) {
             console.log('not found');
-            return res
-                .status(404)
-                .json({ message: `User with ID ${req.params.id} not found` });
+            return res.status(404).json({
+                message: `Warehouse with ID ${req.params.id} not found`,
+            });
         }
-
+        console.log('Deleted ID: ', req.params.id);
         // No Content response
         res.sendStatus(204);
     } catch (error) {
