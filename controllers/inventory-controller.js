26f2f41a9189b7e296d616b535f23b081bb32bf9
Nicolas Martinez <nicmart@gmail.com>
2024-05-22 13:24:36 -0700

    add warehouse_name using join query

--- controllers/inventory-controller.js
+++ controllers/inventory-controller.js
@@ -3,13 +3,15 @@ const knex = require("knex")(require("../knexfile"));
 // get all inventory items
 const index = async (_req, res) => {
   try {
-    const data = await knex("inventories");
-    res.status(200).json(data);
+    const items = await knex("inventories").join("warehouses", "warehouses.id", "inventories.warehouse_id").select("inventories.id", "warehouses.warehouse_name", "inventories.item_name", "inventories.description", "inventories.category", "inventories.status", "inventories.quantity");
+    res.status(200).json(items);
   } catch (err) {
     console.log(err)
     res.status(400).send("Error retrieving inventory items.");
   }
 };
+
+
 
 module.exports = {
   index,
