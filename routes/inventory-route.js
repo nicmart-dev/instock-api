4681499b318a42462bb46df87d0742b772a6a693
Benjamin Ayesu <ben.ayesu.attah@gmail.com>
2024-05-23 16:05:22 -0700

    Update inventory-route.js

--- routes/inventory-route.js
+++ routes/inventory-route.js
@@ -1,13 +1,13 @@
-const router = require("express").Router();
-const inventoryController = require("../controllers/inventory-controller");
+const router = require('express').Router();
+const inventoryController = require('../controllers/inventory-controller');
 
-require("dotenv").config(); // Ensure environment variables are available
+require('dotenv').config(); // Ensure environment variables are available
 const { PORT, BACKEND_URL, NODE_ENV } = process.env; // Destructure process.env
 
-router.route("/").get(inventoryController.index).post(inventoryController.add);
+router.route('/').get(inventoryController.index).post(inventoryController.add);
 
 router
-  .route("/:id")
+  .route('/:id')
   .get(inventoryController.findOne)
   .delete(inventoryController.remove); // get single inventory item
 
