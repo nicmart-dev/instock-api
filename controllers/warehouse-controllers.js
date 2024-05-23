eb5c7ad090c8f5081564db2ae1fd0e83ff484344
Green-Eggs-And-SAM <sam.john.alexander@gmail.com>
2024-05-23 09:37:44 -0700

    merge conflicts resolved + intentories -> inventory

--- controllers/warehouse-controllers.js
+++ controllers/warehouse-controllers.js
@@ -1,31 +1,31 @@
 //WAREHOUSE CONTROLLERS
 
-const knex = require("knex")(require("../knexfile"));
+const knex = require('knex')(require('../knexfile'));
 
 const remove = async (req, res) => {
-  try {
-    console.log("Finding id: ", req.params.id);
-    const warehouseDeleted = await knex("warehouses")
-      .where({ id: req.params.id })
-      .delete();
+    try {
+        console.log('Finding id: ', req.params.id);
+        const warehouseDeleted = await knex('warehouses')
+            .where({ id: req.params.id })
+            .delete();
 
-    if (warehouseDeleted === 0) {
-      console.log("not found");
-      return res.status(404).json({
-        message: `Warehouse with ID ${req.params.id} not found`,
-      });
+        if (warehouseDeleted === 0) {
+            console.log('not found');
+            return res.status(404).json({
+                message: `Warehouse with ID ${req.params.id} not found`,
+            });
+        }
+        console.log('Deleted ID: ', req.params.id);
+        // No Content response
+        res.sendStatus(204);
+    } catch (error) {
+        res.status(500).json({
+            message: `Unable to delete user: ${error}`,
+        });
     }
-    console.log("Deleted ID: ", req.params.id);
-    // No Content response
-    res.sendStatus(204);
-  } catch (error) {
-    res.status(500).json({
-      message: `Unable to delete user: ${error}`,
-    });
-  }
 };
 
-const inventories = async (req, res) => {
+const inventory = async (req, res) => {
     try {
         const posts = await knex('warehouses')
             .join('inventories', 'inventories.warehouse_id', 'warehouses.id')
@@ -48,5 +48,5 @@ const inventories = async (req, res) => {
 
 module.exports = {
     remove,
-    inventories,
+    inventory,
 };
