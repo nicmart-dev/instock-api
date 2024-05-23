4b5141d825a605bc6bf492db62d698ba1a1af88f
Benjamin Ayesu <ben.ayesu.attah@gmail.com>
2024-05-23 16:10:03 -0700

    Update warehouses-route.js

--- routes/warehouses-route.js
+++ routes/warehouses-route.js
@@ -5,9 +5,9 @@ const warehouseController = require('../controllers/warehouse-controller');
 require('dotenv').config(); // Ensure environment variables are available
 const { PORT, BACKEND_URL, NODE_ENV } = process.env; // Destructure process.env
 
-router.route('/').get(warehouseController.index);
+router.route("/").get(warehouseController.index);
 
-router.route("/:id").delete(warehouseController.remove);
+router.route('/:id').delete(warehouseController.remove);
 
 router.route('/:id/inventory').get(warehouseController.inventory);
 
